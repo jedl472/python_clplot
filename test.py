f0e7000199a72f0461154915d95b556b88110cf0
jedl472 <jodltomas6@gmail.com>
2025-01-16 21:16:31 +0100

    repo init

---
+++ test.py
@@ -0,0 +1,19 @@
+from python_clplot import *
+
+print("------ canvas ------")
+
+test1 = Canvas(10, 10) #x a y velikost platna
+test1.content.append(Point(7, 8, "L")) #x, y a znak
+test1.content.append(Point(8, 6, "X"))
+test1.content.append(Rect(1, 2, 6, 4)) #x, y prvniho bodu, x, y druheho bodu a znak
+test1.content.append(Rect(2, 6, 5, 9, "$"))
+test1.draw()
+print("\n------ bar graph 1 ------")
+test = bar_graph(2, 10) # 1. hodnota je sirka sloupcu a druha je vyska grafu
+test.graphValues = [6, 5, 10, 17, 24, 7, 11, 15]
+test.graphLabels = ["A", "B", "C", "D", "E", "F", "Ahoj"]
+test.drawGraph()
+print("\n------ bar graph 2 ------")
+test.bar_width = 1
+test.sizey = 20
+test.drawGraph()
